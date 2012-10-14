4cd7cde956f01af515a11987cb7035489d217cc7
ColdFreak <madfrogme@gmail.com>
2012-10-14 16:59:59 +0900

    first OS try

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <system.h>
+
+/* You will need to code these up yourself*/
+unsigned char *memcpy(unsigned char *dest, const unsigned cahr *src, int count) {
+    /* Add code here to copy 'count' bytes of data
+     * from 'src' to 'dest', finally return 'dest'
+     * */
+}
+
+unsigned char *memset(unsigned char *dest, unsigned char val, int count) {
+    /* Add code here to set 'count' bytes in 'dest' to
+     * 'val'. Again, return 'dest'
+     * */
+}
+
+unsigned short *memsetw(unsigned short *dest, unsigned short val, int count) {
+    /* Same as above, but this time, we're working
+     * with a 16-bit 'val' and dest pointer.
+     * Your code can be an exact copy of the above,
+     * provided that your local variables if any, 
+     * are unsigned short */
+}
+
+
---
+++ start.asm
@@ -0,0 +1,53 @@
+[BITS 32]
+global start
+start:
+    mov esp, _sys_stack ; This points the stack to our new stack area
+    jmp stublet
+
+; This part MUST be 4byte aligned, so we solve that issue using 'ALIGN 4'
+mboot:
+    ; Multiboot macros to make a few lines later more readable
+    MULTIBOOT_PAGE_ALIGN    equ 1<<0
+    MULTIBOOT_MEMORY_INFO   equ 1<<1
+    MULTIBOOT_AOUT_KLUDGE   equ 1<<16
+    MULTIBOOT_HEADER_MAGIC  equ 0x1BADB002
+    MULTIBOOT_HEADERFLAGS   equ MULTIBOOT_PAGE_ALIGN | MULTIBOOT_MEMORY_INFO | MULTIBOOT_AOUT_KLUDGE
+    MULTIBOOT_CHECKSUM      equ -(MULTIBOOT_HEADER_MAGIC+MULTIBOOT_HEADERFLAGS)
+    EXTERN code, bss, end
+
+    ; This is the GRUB Multiboot header. A boot signature
+    dd MULTIBOOT_HEADER_MAGIC
+    dd MULTIBOOT_HEADERFLAGS
+    dd MULTIBOOT_CHECKSUM
+
+    ; AOUT kludge - must be physical addresses. make a note of these:
+    ; The linker script filess in the data for these ones
+    dd mboot
+    dd code
+    dd bss
+    dd end
+    dd start
+
+; This is and endless loop here. Make a note of this: Later on, we will
+; insert an 'exter _main' followed by 'call _main', right
+; before the 'jmp $'.
+stublet:
+    extern _main
+    call _main
+    jmp $
+
+
+; Shortly we will add code for loading the GDT right here!
+
+
+; In just a few pages in this tutorial, we will add our Interrupt
+; Service Routines(ISRs) right here!
+
+
+; Here is the definition of our BSS section. Right now, we'll use
+; it just to store the stack. Remember that a stack actually grows
+; downwards, so we declare the size of the data before declaring
+; the identifier '_sys_stack'
+SECTION .bss
+    resb 8192   ; This reserves 8K bytes of memory here
+_sys_stack:
