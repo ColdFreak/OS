55ba8d1968d24a12d7b7497015acd49ba145f454
ColdFreak <madfrogme@gmail.com>
2012-10-14 21:37:35 +0900

    add outpotb

--- main.c
+++ main.c
@@ -34,9 +34,18 @@ int strlen(const char *str) {
  * as the keyboard. We are using what is called
  * 'inline assembly' in these routines to actually
  *  do the work */
-unsigned char inport (unsigned short _port) {
+unsigned char inportb (unsigned short _port) {
     unsigned char rv;
     /*"N" : Const in range 0 to 255(for out instruction) */
     __asm__ __volatile__("inb %1, %0" : "=a" (rv): "dN"(_port));
     return rv;
+}
+
+/* We will use this to write to I/O ports to send
+ * bytes to devices. This will be used in the next 
+ * tutorial for changing the textmode cursor
+ * position. Again, we use some inline assembly for the 
+ * stuff that simply cannot be done in C*/
+void outportb(unsigned short _port, unsigned char _data) {
+    __asm__ __volatile__("outb %1, %0" : : "dN"(_port), "a" (_data));
 }
