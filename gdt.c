1864e7baa4f952e91a6866105e862a7d1031494c
ColdFreak <madfrogme@gmail.com>
2012-10-21 17:28:06 +0900

    add gdt_set_gate

--- gdt.c
+++ gdt.c
@@ -29,3 +29,19 @@ struct gdt_ptr gp;
  * reload the new segment registers */
 extern void gdt_flush();
 
+/* Setup a descriptor in the Global Descriptor table */
+void 
+gdt_set_gate(int number, unsigned long base, unsigned long limit, unsigned char access, unsigned char gran) {
+	gdt[num].base_low = (base & 0xFFFF);
+	gdt[num].base_middle = (base >> 16) & 0xFF;
+	gdt[num].base_high = (base >> 24) & 0xFF;
+
+	/* Setup the descriptor limits*/
+	gdt[num].limit_low = (limit & 0xFFFF);
+	gdt[num].granularity = ((limit >> 16) & 0x0F);
+
+	/* Finally, set up the granularity and access flags */
+	gdt[num].granularity |= (gran & 0xF0);
+	gdt[num].access = access;
+}
+
